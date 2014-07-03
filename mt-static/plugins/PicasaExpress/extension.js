ddd23ae8605948ba65ba4ef22c1054ca4e8d0174
Taku AMANO <taku@taaas.jp>
2014-07-03 14:58:42 +0900

    Show in WYSIWYG mode and Source mode.

--- mt-static/plugins/PicasaExpress/extension.js
+++ mt-static/plugins/PicasaExpress/extension.js
@@ -1,20 +1,45 @@
-tinymce.PluginManager.add('picasaexpress', function(ed, url) {
-    // Add a button that opens a window
-    ed.addButton('picasaexpress', {
-        text: 'Picasa Express',
-        icon: 'https://votecharlie.com/mt/mt-static/plugins/PicasaExpress/toolbar-picasa-express.gif',
-        onclick: function() {
-            var blogId = jQuery('#blog-id').val() || 0;
-            // Open window
-            ed.windowManager.open({
-      					//url : '/test.htm',
-      					url : ScriptURI + '?__mode=pwa_albums&blog_id=' + blogId + '&dialog_view=1',
-                title: 'Picasa Express'
-            });
-        }
-    });
+tinymce.create('tinymce.plugins.picasaexpress', {
+    init: function(ed, url) {
+        this.buttonIDs = {};
 
+        // Add a button that opens a window
+        ed.addButton('picasaexpress', {
+            text: 'Picasa Express',
+            icon: 'https://votecharlie.com/mt/mt-static/plugins/PicasaExpress/toolbar-picasa-express.gif',
+            onclick: function() {
+                var blogId = jQuery('#blog-id').val() || 0;
+                // Open window
+                ed.windowManager.open({
+      					    //url : '/test.htm',
+      					    url : ScriptURI + '?__mode=pwa_albums&blog_id=' + blogId + '&dialog_view=1',
+                    title: 'Picasa Express'
+                });
+            }
+        });
+    },
+
+    createControl : function(name, cm) {
+        var editor = cm.editor;
+        var ctrl   = editor.buttons[name];
+
+        if (name == 'picasaexpress') {
+            if (! this.buttonIDs[name]) {
+                this.buttonIDs[name] = [];
+            }
+
+            var id = name + '_' + this.buttonIDs[name].length;
+            this.buttonIDs[name].push(id);
+
+            return cm.createButton(id, tinymce.extend({}, ctrl, {
+                'class': 'mce_' + name
+            }));
+        }
+
+        return null;
+    }
 });
+
+tinymce.PluginManager.add('picasaexpress', tinymce.plugins.picasaexpress);
 
 (function($) {
 
@@ -22,8 +47,8 @@ var config  = MT.Editor.TinyMCE.config;
 
 $.extend(config, {
     plugins: config.plugins + ',picasaexpress',
-    plugin_mt_source_buttons1: 'picasaexpress,|,|,|,|,' + (config.plugin_mt_source_buttons1 || ''),
-    plugin_mt_common_buttons1: 'picasaexpress,|,' + (config.plugin_mt_common_buttons1 || '')
+    plugin_mt_wysiwyg_buttons1: 'picasaexpress,|,' + (config.plugin_mt_wysiwyg_buttons1 || ''),
+    plugin_mt_source_buttons1: 'picasaexpress,|,' + (config.plugin_mt_source_buttons1 || '')
 });
 
 var blogId = jQuery('#blog-id').val() || 0;
